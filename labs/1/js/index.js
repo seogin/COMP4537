2e6a822f25e3bdfae53ab55dc21d9e5f697f2e1b
seogin <hsihsj94@gmail.com>
2025-09-13 12:10:51 -0700

    Add message population functionality and remove unused script

---
+++ labs/1/js/index.js
@@ -0,0 +1,5 @@
+    // Populate from messages
+    document.getElementById("title").textContent = USER_MESSAGES.INDEX_PAGE_TITLE;
+    document.getElementById("studentLine").innerHTML = USER_MESSAGES.INDEX_STUDENT_LINE;
+    document.getElementById("linkWriter").textContent = USER_MESSAGES.INDEX_LINK_WRITER;
+    document.getElementById("linkReader").textContent = USER_MESSAGES.INDEX_LINK_READER;
